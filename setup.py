bed524a94c523ecbe1b2d5442736f292df9f39fa
psusarla <praneeth.susarla@oulu.fi>
2019-10-27 11:56:35 +0200

    package installation setup

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(name='gym_uav',
       version='0.0.1',
       install_requires=['gym', 'numpy']#And any other dependencies required
+)
+
+setup(name='drone_rl',
+      version='0.0.1',
+      packages=find_packages(),
+      install_requires=required#And any other dependencies required
 )
